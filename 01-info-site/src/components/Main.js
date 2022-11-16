45dea37461c422670884cd5a79ae177f4bbe702a
0xMastxr <0xmastxr@gmail.com>
2022-11-16 13:02:47 +0100

    initial guided project

---
+++ 01-info-site/src/components/Main.js
@@ -0,0 +1,19 @@
+import React from "react"
+import reactHalfLogo from "../images/reactjs-icon-half.png"
+
+export default function Main() {
+    return (
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>
+                    Powers thousands of enterprise apps, including mobile apps
+                </li>
+            </ul>
+        </main>
+    )
+}
---
+++ 01-info-site/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import reactLogo from "../images/react-logo.png"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={reactLogo} />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
