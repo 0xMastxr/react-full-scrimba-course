eef90c755465d37f42d2757f437c677af1049f76
0xMastxr <0xmastxr@gmail.com>
2022-11-25 10:19:34 +0100

    last guided projects

---
+++ 06-info-site-darkmode/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react"
+import reactIcon from "../images/react-icon-small.png"
+
+export default function Navbar(props) {
+    return (
+        <nav className={props.darkMode ? "dark" : ""}>
+            <img className="nav--logo_icon" src={reactIcon} />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+
+            <div className="toggler">
+                <p className="toggler--light">Light</p>
+                <div className="toggler--slider" onClick={props.toggleDarkMode}>
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+        </nav>
+    )
+}
---
+++ 06-notes-app/src/App.js
@@ -0,0 +1,101 @@
+import React from "react"
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor"
+import { data } from "./data" //Used just for testing!
+import Split from "react-split"
+import { nanoid } from "nanoid"
+
+export default function App() {
+    const [notes, setNotes] = React.useState(
+        () => JSON.parse(localStorage.getItem("notes")) || [] // This is a lazy initialization
+        // It will just run that code one time (on the render) and not on every change
+        // That's why we use a function in there
+    )
+    const [currentNoteId, setCurrentNoteId] = React.useState(
+        (notes[0] && notes[0].id) || ""
+    )
+
+    React.useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes))
+    }, [notes])
+
+    function createNewNote() {
+        const newNote = {
+            id: nanoid(),
+            body: "# Type your markdown note's title here",
+        }
+        setNotes((prevNotes) => [newNote, ...prevNotes])
+        setCurrentNoteId(newNote.id)
+    }
+
+    function updateNote(text) {
+        // Put the most recent modified note at the top
+        setNotes((oldNotes) => {
+            const newArray = []
+            for (let i = 0; i < oldNotes.length; i++) {
+                const oldNote = oldNotes[i]
+                if (oldNote.id === currentNoteId) {
+                    newArray.unshift({ ...oldNote, body: text })
+                } else {
+                    newArray.push(oldNote)
+                }
+            }
+            return newArray
+        })
+    }
+
+    // This do not rearrange the notes on the sidebar
+    //     setNotes((oldNotes) =>
+    //     oldNotes.map((oldNote) => {
+    //         return oldNote.id === currentNoteId
+    //             ? { ...oldNote, body: text }
+    //             : oldNote
+    //     })
+    // )
+
+    function deleteNote(event, noteId) {
+        event.stopPropagation()
+        setNotes((oldNotes) => oldNotes.filter((note) => note.id !== noteId))
+    }
+
+    function findCurrentNote() {
+        return (
+            notes.find((note) => {
+                return note.id === currentNoteId
+            }) || notes[0]
+        )
+    }
+
+    return (
+        <main>
+            {notes.length > 0 ? (
+                <Split
+                    sizes={[30, 70]}
+                    direction="horizontal"
+                    className="split"
+                >
+                    <Sidebar
+                        notes={notes}
+                        currentNote={findCurrentNote()}
+                        setCurrentNoteId={setCurrentNoteId}
+                        newNote={createNewNote}
+                        deleteNote={deleteNote}
+                    />
+                    {currentNoteId && notes.length > 0 && (
+                        <Editor
+                            currentNote={findCurrentNote()}
+                            updateNote={updateNote}
+                        />
+                    )}
+                </Split>
+            ) : (
+                <div className="no-notes">
+                    <h1>You have no notes</h1>
+                    <button className="first-note" onClick={createNewNote}>
+                        Create one now
+                    </button>
+                </div>
+            )}
+        </main>
+    )
+}
